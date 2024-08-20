9ab2b7619edf24c1631984e30b7d870f1f0bc1c5
Tien Chung <tchung7171@gmail.com>
2024-08-20 14:07:29 -0400

    Added shuffleAnswers to randomize the order.

---
+++ src/helper.js
@@ -0,0 +1,11 @@
+export const shuffleAnswers = question => {
+    const unshuffleAnswers = [
+        question.correctAnswer,
+        ...question.incorrectAnswers,
+    ]
+    
+    return unshuffleAnswers.map(unshuffleAnswer => ({
+        sort: Math.random(),
+        value: unshuffleAnswer,
+    })).sort((a,b) => a.sort - b.sort).map((a) => a.value);
+}
